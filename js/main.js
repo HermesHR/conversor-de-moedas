51e4a7dcca680afbad7f4ecf8f139673befa7be8
HermesHR <filhos.hr@gmail.com>
2022-10-05 16:54:17 -0300

    add convert dollar to euro

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
-function Converter() {
+function Real() {
     var valorElemento = document.getElementById("valor");
     var valor = valorElemento.value;
     var valorEmDolarNumerico = parseFloat(valor);
     console.log(valorEmDolarNumerico);
 
-    var valorEmReal = valorEmDolarNumerico * 5;
+    var valorEmReal = valorEmDolarNumerico * 5.17;
     console.log(valorEmReal);
 
     var elementoValorConvertido = document.getElementById("valorConvertido");
-    var valorConvertido = "O resultado em real é R$ " + valorEmReal;
+    var valorConvertido = "O resultado em Real é R$ " + valorEmReal;
+
+    elementoValorConvertido.innerHTML = valorConvertido;
+}
+
+function Euro() {
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmDolarNumerico = parseFloat(valor);
+    console.log(valorEmDolarNumerico);
+
+    var valorEmEuro = valorEmDolarNumerico * 1.01;
+    console.log(valorEmEuro);
+
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "O resultado em Euro é R$ " + valorEmEuro.toFixed(2);
 
     elementoValorConvertido.innerHTML = valorConvertido;
 }
